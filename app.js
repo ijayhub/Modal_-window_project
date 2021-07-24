659db4238ce27b8123c7dd95d6d11d95411bbf85
ijaylab <ijeonyi@gmail.com>
2021-07-24 15:43:57 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const openBtn = document.querySelector('.open');
-//console.log(openBtn)
-const modalContainer = document.querySelector('.modal-container');
-const closeBtn = document.querySelector('.modal-btn');
+const openBtn=document.querySelector('.open');
+const modalContainer= document.querySelector('.modal-container');
+const closeBtn=document.querySelector('.btn');
 
+// add the eventListener
 openBtn.addEventListener('click', function(){
     modalContainer.classList.add('show');
-});
+
+})
 
 closeBtn.addEventListener('click', function(){
     modalContainer.classList.remove('show');
